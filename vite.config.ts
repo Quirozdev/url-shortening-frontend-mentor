90f8f56cb7d85ed8843635699baebd1901337a8c
Quirozdev <luisdevv232@gmail.com>
2026-05-03 14:32:17 -0700

    set base path for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/url-shortening-frontend-mentor/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
